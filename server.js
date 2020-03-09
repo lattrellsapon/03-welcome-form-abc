5b6e0525a7c2b6dfdced60fb00eed15c533d39b2
lattrellsapon <lattrellsapon@gmail.com>
2020-03-09 22:32:02 +1300

    deploy

--- server.js
+++ server.js
@@ -69,7 +69,9 @@ app.post('/send', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
