17b27d9fe2e7c911d9baf91405ea77af7fd23f0f
lattrellsapon <lattrellsapon@gmail.com>
2020-03-09 22:29:44 +1300

    deploy now

---
+++ client/build/precache-manifest.61e80f74c3b80ec350c8bce00318c095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fbf55bfa2ef3daa8a3d98dcef83cba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b1953b779e46b9df285",
+    "url": "/static/css/main.e8a62537.chunk.css"
+  },
+  {
+    "revision": "aa4365489b11b4142267",
+    "url": "/static/js/2.794425a0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.794425a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1953b779e46b9df285",
+    "url": "/static/js/main.fda4f30b.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
